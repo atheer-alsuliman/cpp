6635f0084f824dafba1b39a406bd4ed3695c134b
atheer-alsuliman <108820408+atheer-alsuliman@users.noreply.github.com>
2023-09-06 16:54:57 +0300

    Add files via upload

---
+++ App.xaml.cpp
@@ -0,0 +1,30 @@
+﻿#include  <iostream> 
+
+
+
+
+using namespace std;
+int main()
+{
+	// input from the user
+
+	int width , length ,area ;
+	
+
+	cout << "please enter rectangle width\n";
+	cin >> width;
+
+	cout << "please enter your rectangle length\n";
+	cin >> length;
+
+	//process
+
+	area = width * length;
+
+	//output
+	cout << area;
+	return 0;
+
+
+}
+
